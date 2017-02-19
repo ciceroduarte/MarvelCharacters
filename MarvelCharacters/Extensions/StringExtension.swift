805fe6c12ed98d34a09e753755bde2c550ef556b
CiceroDuarte <ciceroduarte1@gmail.com>
2017-02-19 19:54:28 -0300

    Adding Bridging-Header and StringExtension

---
+++ MarvelCharacters/Extensions/StringExtension.swift
@@ -0,0 +1,9 @@
+//
+//  StringExtension.swift
+//  MarvelCharacters
+//
+//  Created by Cicero on 19/02/17.
+//  Copyright © 2017 ciceroduarte. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelCharacters/Resources/MarvelCharacters-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MarvelCharacters-Bridging-Header.h
+//  MarvelCharacters
+//
+//  Created by Cicero on 19/02/17.
+//  Copyright © 2017 ciceroduarte. All rights reserved.
+//
+
+#ifndef MarvelCharacters_Bridging_Header_h
+#define MarvelCharacters_Bridging_Header_h
+
+
+#endif /* MarvelCharacters_Bridging_Header_h */

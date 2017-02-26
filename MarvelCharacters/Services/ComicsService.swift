639dd60713102f1d2dfda6cacfaf8c10c456fbde
CiceroDuarte <ciceroduarte1@gmail.com>
2017-02-26 18:36:31 -0300

    Adding comic service

--- MarvelCharacters/Services/ComicsService.swift
+++ MarvelCharacters/Services/ComicsService.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class ComicsService: ServiceBase {
+    
+    func fetch (withURL url: URL, completionHandler: @escaping (Result<[Comic], FetchError>) -> Void) {
+        
+        fetch(listOf: Comic.self, withURL:url) { (result) in
+            switch result {
+            case .success(let comics):
+                completionHandler(Result.success(comics))
+            case .failure(let error):
+                completionHandler(Result.failure(error))
+            }
+        }
+    }
+}

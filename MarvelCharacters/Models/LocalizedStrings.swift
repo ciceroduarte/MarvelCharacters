a8801fc0bf83dc73928a16cdf852c1fd87e5441a
CiceroDuarte <ciceroduarte1@gmail.com>
2017-02-26 19:52:44 -0300

    Adding LocalizedStrings

---
+++ MarvelCharacters/Models/LocalizedStrings.swift
@@ -0,0 +1,14 @@
+//
+//  LocalizedStrings.swift
+//  MarvelCharacters
+//
+//  Created by Cicero on 26/02/17.
+//  Copyright © 2017 ciceroduarte. All rights reserved.
+//
+
+import Foundation
+
+struct LocalizedStrings {
+    static let comics: String = NSLocalizedString("Comics", comment: "")
+    static let series: String = NSLocalizedString("Series", comment: "")
+}

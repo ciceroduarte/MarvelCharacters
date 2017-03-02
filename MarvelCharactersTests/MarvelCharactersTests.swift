522c300b36d04da901d1b8430db8c28304188077
CiceroDuarte <ciceroduarte1@gmail.com>
2017-03-02 20:49:56 -0300

    Adding KIF

--- MarvelCharactersTests/MarvelCharactersTests.swift
+++ MarvelCharactersTests/MarvelCharactersTests.swift
@@ -7,10 +7,40 @@
 //
 
 import XCTest
+import KIF
 @testable import MarvelCharacters
 
-class MarvelCharactersTests: XCTestCase {
+class MarvelCharactersTests: KIFTestCase {
 
+    var homeViewController: HomeViewController?
+    
+    override func beforeEach() {
+        stopTestsOnFirstBigFailure = true
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let homeViewModel = HomeViewModel()
+        let mockSession = MockURLSession()
+        
+        let expectedData = ("{\"data\": { \"offset\": 0, \"limit\": 20, \"total\": 30920," +
+            "\"count\": 20, \"results\": [{\"name\": \"cicero\", \"description\": \"description\"," +
+            "\"comics\": { \"collectionURI\": \"http://www.google.com.br\" }," +
+            "\"series\": { \"collectionURI\": \"http://www.google.com.br\" }," +
+            "\"thumbnail\":{ \"path\": \"http://www.google.com.br\", \"extension\": \"png\"} }]}}"
+            ).data(using: String.Encoding.utf8)
+        mockSession.nextData = expectedData
+        
+        homeViewModel.charactersService.session = mockSession
+        let navigationDelegate = NavigationDelegate()
+        
+        homeViewController = HomeViewController(withHomeViewModel: homeViewModel)
+        let navigationController = UINavigationController(rootViewController: homeViewController!)
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationBar.barTintColor = .black
+        navigationController.navigationBar.tintColor = .white
+        navigationController.delegate = navigationDelegate
+        appDelegate?.window?.rootViewController = navigationController
+    }
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,5 +49,19 @@ class MarvelCharactersTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testCharacterDetail() {
+        //waitForTimeInterval:(NSTimeInterval)timeInterval
+        guard let collectionView = tester()
+            .waitForView(withAccessibilityLabel: "collectionView") as? UICollectionView else {
+            XCTFail("CollectionView not found")
+            return
+        }
+        
+        XCTAssertNotNil(collectionView)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tester().tapItem(at: indexPath, in: collectionView)
     }
 }
